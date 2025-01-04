5085c9da3645e0bfed5094440e2bac8f440b955f
Arron Robinson <arron.robinson@hva.nl>
2025-01-04 20:12:49 +0100

    aaa

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,16 @@
 import createMiddleware from 'next-intl/middleware';
- 
+
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'nl', "it"],
- 
-  // Used when no locale matches
-  defaultLocale: 'nl'
+  locales: ['en', 'nl', 'it'],
 
-  
+  // Default locale if none matches
+  defaultLocale: 'nl',
 });
- 
-export const config = {
-  // Match only internationalized pathnames
-  matcher: ['/', '/(nl|en|it)/:path*', '/((?!_next|_vercel|.*\\..*).*)']
-};
 
-// export const config = {
-//   matcher: ['/:locale(.*)', '/'], // Match all paths except root and locale paths
-// };
+export const config = {
+  // Match paths starting with a locale or the root path
+  matcher: [
+    '/((?!_next|_vercel|.*\\..*).*)', // Exclude Next.js internals and static files
+  ],
+};
